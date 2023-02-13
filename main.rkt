d0490cd928c5ee96282a4c6cb1b13f94a41d93d2
Antigen-1 <zhanghao@antigen.top>
2023-02-14 00:59:33 +0800

    update the macro

--- main.rkt
+++ main.rkt
@@ -2,23 +2,25 @@
 (require (for-syntax racket/base))
 (provide (all-defined-out))
 
-(define-syntax-rule
-  (define-data
-    name
-    (share ...)
-    (primitive ...)
-    ((operation op) ...)
-    ((abstraction ab) ...))
-  (module name racket/base
-    (module represent racket/base
-      (require share ... primitive ...)
-      (provide operation ...)
-      (define operation op)
-      ...)
-    (module abstract racket/base
-      (require (submod ".." represent) share ...)
-      (provide abstraction ...)
-      (define abstraction ab)
-      ...)
-    (require 'represent 'abstract)
-    (provide (all-from-out 'represent 'abstract))))
+(define-syntax (define-data stx)
+  (syntax-case stx (lib operation abstraction)
+      ((_ name
+          (lib init (primitive ...))
+          (operation (op-name op-body) ...)
+          (abstraction (ab-name ab-body) ...))
+       (with-syntax (((op-alias ...) (generate-temporaries #'(op-name ...))))
+         #'(module name racket/base
+             (module represent init
+               (require primitive ...)
+               (provide op-alias ...)
+               (define op-alias op-body)
+               ...)
+             (module abstract init
+               (require (rename-in (submod ".." represent)
+                                   (op-alias op-name)
+                                   ...))
+               (provide (all-from-out (submod ".." represent)) ab-name ...)
+               (define ab-name ab-body)
+               ...)
+             (require 'abstract)
+             (provide (all-from-out 'abstract)))))))
