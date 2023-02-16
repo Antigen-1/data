77f42fda632cbcd61f5edf4288fe0d46aee2197b
Antigen-1 <zhanghao@antigen.top>
2023-02-16 16:59:50 +0800

    add a temporary space

--- main.rkt
+++ main.rkt
@@ -11,13 +11,18 @@
        (let* ((make-id (lambda (str) (string->symbol (format "~a:~a" (syntax->datum #'name) str))))
               (rp-id (make-id "representation"))
               (ab-id (make-id "abstraction"))
+              (tmp-id (string->symbol (symbol->string (gensym "tmp"))))
+              (add-tmp (lambda (stx) ((make-interned-syntax-introducer tmp-id) stx 'add)))
               (add-rp (lambda (stx) ((make-interned-syntax-introducer rp-id) stx 'add)))
               (add-ab (lambda (stx) ((make-interned-syntax-introducer ab-id) stx 'add))))
-         ((compose add-rp add-ab)
-          #`(begin
-              (splicing-let () (local-require primitive ...) (define rp-name rp-body) ...)
-              (splicing-let () (define ab-name ab-body) ...)
-              (provide (for-space #,rp-id rp-name ...)
-                       (for-space #,ab-id ab-name ...)
+         (with-syntax (((rp-space-name ab-space-name tmp-space-name) (map (lambda (sym) (datum->syntax #'stx sym)) (list rp-id ab-id tmp-id)))
+                       ((rp-name ... rp-body ...) (map (compose add-rp add-tmp) (syntax->list #'(rp-name ... rp-body ...))))
+                       ((ab-name ... ab-body ...) (map (compose add-ab add-rp) (syntax->list #'(ab-name ... ab-body ...)))))
+          #'(begin
+              (require (for-space tmp-space-name primitive ...))
+              (define rp-name rp-body) ...
+              (define ab-name ab-body) ...
+              (provide (for-space rp-space-name rp-name ...)
+                       (for-space ab-space-name ab-name ...)
                        (for-space #f rp-name ... ab-name ...))
               ))))))
